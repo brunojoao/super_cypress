2ced8bd696bf81f8ea042e74f1d2aa41a1376c59
brunojoao <bruno.garcia@essentia.com.br>
2024-09-19 16:09:18 -0300

    abre menu de receitas

--- cypress/e2e/teste_geral_sm.cy.js
+++ cypress/e2e/teste_geral_sm.cy.js
@@ -7,5 +7,18 @@ describe('template spec', () => {
     cy.get('input[name="password"]').type('789123')
 
     cy.get('button[type="submit"]').click()
+
+    cy.wait(3000)
+
+    cy.get('div.modal-close').then(($modal) => {
+      if ($modal.length) {
+        cy.wrap($modal).click()
+      }
+    })
+
+    cy.get('ul.side-menu li')
+      .find('i.fa-file-text-o')
+      .parents('li')
+      .click()
   })
 })
